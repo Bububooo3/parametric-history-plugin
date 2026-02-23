626aaedaad9783b61aecb98cb44cf27a30fc91cd
Richard Scott <bububooo3@gmail.com>
2026-02-23 19:28:58 +0000

    planning

--- plugin.lua
+++ plugin.lua
@@ -1,1 +1,9 @@
-print("Hello, world!")
+-- Detect new baseparts
+----> workspace:descendent added/removing
+
+-- Detect changes in basepart properties
+----> Predefined fxn connected to propertyChanged signal
+
+-- Store data as "keyframes"
+----> Long buffer for each instance
+----> Parsing fxn
