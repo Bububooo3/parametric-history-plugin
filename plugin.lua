1bee26f7d17436cb7ec695b72b71e5c3ca5ef6ad
Richard Scott <bububooo3@gmail.com>
2026-02-24 00:01:04 +0000

    check-in

--- plugin.lua
+++ plugin.lua
@@ -23,12 +23,22 @@ type FrameData = {
 local HS = game:GetService("HttpService")
 local CS = game:GetService("CollectionService")
 
+-- LinkedList
+local root: number -- reverse linkedlist inside of our hashmaps (first capture)
+local head: number -- like the most recent capture
+local current: number -- our current node
+
+-- Magic numbers
+local destructionCode = -2 -- if #cframe == 1 and [1] is -2 then we know this is the end of the line and we handle it accordingly
+
 -- Storage
-local tracked: { [string]: { buffer } } = {}
+local tracked: { [string]: { [number]: buffer } } = {}
 
 setmetatable(tracked, {
 	__index = function(t, k)
-		rawset(t, k, {})
+		local newItem = {}
+		rawset(t, k, newItem)
+		return newItem
 	end,
 })
 
@@ -37,11 +47,39 @@ local function getFrameData(token: buffer) end
 
 local function tokenizeFrameData(data: FrameData) end
 
-local function capture(UID: string, description: string)
-	local part: BasePart = CS:GetTagged(UID)[1]
-	local ref = tracked[UID] -- so what we wanna do is have each UID in tracked contain a framedata history as a table of buffers
+local function updateLog() end
 
-	description = (not description or description == "") and "<Empty>" or description
+local function capture(UID: string, description: string)
+	-- Handle the real deal background stuff up in here
+	local part: BasePart = CS:GetTagged(UID)[1]
+	local ref = tracked[UID] -- so what we wanna do is have each UID in tracked contain a framedata history as a table of buffers w/ unique time as key
+
+	--------------------------------------
+	if not part then -- please pardon this interruption
+		if #ref > 0 then -- the part was destroyed
+			rawset(
+				ref,
+				os.time(),
+				tokenizeFrameData({
+					cframe = {destructionCode},
+					scale = Vector3.zero,
+					cancollide = false,
+					anchored = false,
+					color3 = 0,
+					tagline = description or `Removed item with ID: <{UID}> from Workspace`,
+				})
+			)
+
+			return
+		end
+
+		-- if we're atp, then this thing never even existed
+		warn(`Failed to locate item with ID: <{UID}>`)
+		return
+	end
+	--------------------------------------
+
+	description = (not description or description == "") and `Edited {part.Name}` or description
 
 	rawset(
 		ref,
@@ -55,6 +93,8 @@ local function capture(UID: string, description: string)
 			tagline = description,
 		})
 	)
+
+	-- draw it out in the UI down there
 end
 
 -- Connections, detections...
@@ -63,5 +103,12 @@ workspace.DescendantAdded:Connect(function(i: Instance)
 		local UID = HS:GenerateGUID(false)
 		CS:AddTag(i, UID)
 		capture(UID, `Added {i.Name} to Workspace`) -- the first one (initialization)
+	end
+end)
+
+workspace.DescendantRemoving:Connect(function(i: Instance)
+	if i:IsA("BasePart") then
+		local UID = HS:GenerateGUID(false)
+		task.delay(0.05, capture, UID, `Removed {i.Name} from Workspace`) -- might wanna calibrate that delay
 	end
 end)
